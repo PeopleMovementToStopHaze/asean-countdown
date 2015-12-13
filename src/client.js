608afda8fd1d37926cf0e253c98c7fbe3337f1cd
Groupystinks <jason0911520@gmail.com>
2015-12-14 02:22:16 +0800

    react-route: updated base

--- src/client.js
+++ src/client.js
@@ -5,7 +5,7 @@ import App from './App';
 import { createHistory, useBasename } from 'history';
 
 const history = useBasename(createHistory)({
-  basename: '/countdown-clock',
+  basename: '/asean-countdown',
 });
 
 const Routes = (
