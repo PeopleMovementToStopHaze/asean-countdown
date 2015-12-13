f9d8ce764bb031d80a5073a16a29becaf90004d9
Groupystinks <jason0911520@gmail.com>
2015-12-13 17:12:03 +0800

    style: adjusted laptop style

--- src/App.js
+++ src/App.js
@@ -12,27 +12,25 @@ export default class App extends Component {
   render() {
     const deviceDetect = new MobileDetect(window.navigator.userAgent);
     return (
-      <div>
-        <section>
-          <div className={styles.root}></div>
-          <div className={styles.content_container}>
-              <div className={styles.header_container}>
-                <h3 className={styles.header}>Time Left</h3>
-                <h4 className={styles.header_sm}>to Haze-Free ASEAN</h4>
-              </div>
-          </div>
-          {!deviceDetect.mobile() ? (
-          <CountdownTimer
-            deadline={'Oct 29 2020 00:00:50 UTC+0800'}
-            options={{format: 'year-day-hour-minute-second'}}
-            size="180px"
-            fontSize={32}
-            colorFinished={Colors.white}
-            colorGoing={Colors.grey800}
-            textColor={Colors.grey800} />
-          ) : null}
-        </section>
-      </div>
+      <section className={styles.root}>
+        <div className={styles.content_container}>
+            <div className={styles.header_container}>
+              <h3 className={styles.header}>Time Left</h3>
+              <h4 className={styles.header_sm}>to Haze-Free ASEAN</h4>
+            </div>
+        </div>
+        {!deviceDetect.mobile() ? (
+        <CountdownTimer
+          deadline={'Oct 29 2020 00:00:50 UTC+0800'}
+          options={{format: 'year-day-hour-minute-second'}}
+          size="180px"
+          fontSize={32}
+          colorFinished={Colors.white}
+          colorGoing={Colors.grey800}
+          textColor={Colors.grey800} />
+        ) : null}
+        <div className={styles.background}></div>
+      </section>
     );
   }
 }
